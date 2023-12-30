8177e0932b34a59291ce25de3a67ed60dc275e25
Evgeniy Dudarenko <[dudarenko.evgeny@codespace.academy](mailto:)>
2023-12-31 00:13:43 +0300

    first commit

---
+++ frontend/application.php
@@ -0,0 +1,22 @@
+<?php
+// This will generate a Notice that an array cannot b
+$source = [1, 2, 3, 4];
+$filter = [3, 4, [5], 6];
+$result = array_diff($source, $filter);
+// Whereas this is fine, since the objects can cast t
+class S
+{
+    private $v;
+    public function __construct(string $v)
+    {
+        $this->v = $v;
+    }
+    public function __toString()
+    {
+        return $this->v;
+    }
+}
+$source = [new S('a'), new S('b'), new S('c')];
+$filter = [new S('b'), new S('c'), new S('d')];
+$result = array_diff($source, $filter);
+// $result now contains one instance of S('a');
